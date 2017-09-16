c0752ae2e3d1bfbfeee326ef0fbc91d2474e20e9
Adam Freymiller <afreymiller10@gmail.com>
2017-09-16 14:04:38 -0500

    finished generating draws

--- draw_sim.py
+++ draw_sim.py
@@ -12,11 +12,6 @@ teams = {
     "BUR": "CAF", "CIV": "CAF", "EGY": "CAF", "TUN": "CAF"
 }
 
-pot_1 = ["GER", "POR", "BEL", "SUI", "POL", "BRA", "ARG"]
-pot_2 = ["ESP", "WAL", "FRA", "COL", "MEX", "ENG", "URU", "CHI"]
-pot_3 = ["ITA", "HSV", "CRC", "IRN", "USA", "EGY", "TUN", "SER"]
-pot_4 = ["B&H", "JPN", "NGA", "BUR", "AUS", "KOR", "KSA", "CIV"]
-
 def get_random_team_confed_and_count(group, pot):
     rand_i = random.randint(0, len(pot)-1)
     team = pot.pop(rand_i)
@@ -24,8 +19,8 @@ def get_random_team_confed_and_count(group, pot):
     count = group.get(teams[team], 0)
     return team, confed, count
 
-def team_can_be_added_to_group(count, confed):
-    return (count == 0) or (count == 1 and confed == "UEFA")
+def group_is_valid(gp):
+    return (gp["UEFA"] <= 2) and (gp["CONMEBOL"] <= 1) and (gp["CONCACAF"] <= 1) and (gp["CAF"] <= 1) and (gp["AFC"] <= 1)
 
 def update_group(group, team, confed, count):
     group["teams"].append(team)
@@ -33,22 +28,58 @@ def update_group(group, team, confed, count):
     return group
 
 def empty_pot(group, pot):
-    
     team, confed, count = get_random_team_confed_and_count(group, pot)
-    can_be_added = team_can_be_added_to_group(count, confed)
-
-    while not can_be_added:
-        pot.append(team)
-        team, confed, count = get_random_team_confed_and_count(group, pot)
-        can_be_added = team_can_be_added_to_group(count, confed)
-
     group = update_group(group, team, confed, count + 1)
     return group
  
+def check_if_draw_is_valid(a, b, c, d, e, f, g, h):
+    return a and b and c and d and e and f and g and h
 
-        
+def sim_draw():
+    
+    groups = create_groups()
 
-def simulate_draw():
+    _a = group_is_valid(groups[0])
+    _b = group_is_valid(groups[1])
+    _c = group_is_valid(groups[2])
+    _d = group_is_valid(groups[3])
+    _e = group_is_valid(groups[4])
+    _f = group_is_valid(groups[5])
+    _g = group_is_valid(groups[6])
+    _h = group_is_valid(groups[7])
+
+    is_valid = check_if_draw_is_valid(_a, _b, _c, _d, _e, _f, _g, _h)
+
+    while not is_valid:
+        groups = create_groups()
+
+        _a = group_is_valid(groups[0])
+        _b = group_is_valid(groups[1])
+        _c = group_is_valid(groups[2])
+        _d = group_is_valid(groups[3])
+        _e = group_is_valid(groups[4])
+        _f = group_is_valid(groups[5])
+        _g = group_is_valid(groups[6])
+        _h = group_is_valid(groups[7])
+
+        is_valid = check_if_draw_is_valid(_a, _b, _c, _d, _e, _f, _g, _h)
+        #print(is_valid)
+
+    print(groups[0]["teams"])
+    print(groups[1]["teams"])
+    print(groups[2]["teams"])
+    print(groups[3]["teams"])
+    print(groups[4]["teams"])
+    print(groups[5]["teams"])
+    print(groups[6]["teams"])
+    print(groups[7]["teams"])
+
+def create_groups():
+    pot_1 = ["GER", "POR", "BEL", "SUI", "POL", "BRA", "ARG"]
+    pot_2 = ["ESP", "WAL", "FRA", "COL", "MEX", "ENG", "URU", "CHI"]
+    pot_3 = ["ITA", "HSV", "CRC", "IRN", "USA", "EGY", "TUN", "SER"]
+    pot_4 = ["B&H", "JPN", "NGA", "BUR", "AUS", "KOR", "KSA", "CIV"]
+
     A = {"teams": ["RUS"], "CAF": 0, "UEFA": 1, "CONMEBOL": 0, "AFC": 0, "CONCACAF": 0}
     B = {"teams": [], "CAF": 0, "UEFA": 0, "CONMEBOL": 0, "AFC": 0, "CONCACAF": 0}
     C = {"teams": [], "CAF": 0, "UEFA": 0, "CONMEBOL": 0, "AFC": 0, "CONCACAF": 0}
@@ -66,17 +97,34 @@ def simulate_draw():
     G = empty_pot(G, pot_1)
     H = empty_pot(H, pot_1)
 
-    print(A)
-    print(B)
-    print(C)
-    print(D)
-    print(E)
-    print(F)
-    print(G)
-    print(H)
+    A = empty_pot(A, pot_2)
+    B = empty_pot(B, pot_2)
+    C = empty_pot(C, pot_2)
+    D = empty_pot(D, pot_2)
+    E = empty_pot(E, pot_2)
+    F = empty_pot(F, pot_2)
+    G = empty_pot(G, pot_2)
+    H = empty_pot(H, pot_2)
 
+    A = empty_pot(A, pot_3)
+    B = empty_pot(B, pot_3)
+    C = empty_pot(C, pot_3)
+    D = empty_pot(D, pot_3)
+    E = empty_pot(E, pot_3)
+    F = empty_pot(F, pot_3)
+    G = empty_pot(G, pot_3)
+    H = empty_pot(H, pot_3)
 
+    A = empty_pot(A, pot_4)
+    B = empty_pot(B, pot_4)
+    C = empty_pot(C, pot_4)
+    D = empty_pot(D, pot_4)
+    E = empty_pot(E, pot_4)
+    F = empty_pot(F, pot_4)
+    G = empty_pot(G, pot_4)
+    H = empty_pot(H, pot_4)
 
+    return [A, B, C, D, E, F, G, H]
      
 
 def simulate_draw_n_times(n):
