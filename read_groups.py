012745b810547991666a062302edfc22e2f24622
Adam Freymiller <afreymiller10@gmail.com>
2017-09-22 01:31:05 -0500

    converting strings

--- read_groups.py
+++ read_groups.py
@@ -1,5 +1,16 @@
 
-file = open("groups_only.txt", "r")
+def trim(team):
+    x = team.rstrip()
+    y = team.strip(" ")
+    return y
 
-for line in file:
-    print(line.split(","))
+def clean_groups(file_name):
+    file = open(file_name, "r")
+
+    for line in file:
+        split_group = line.split(",")
+        group = [trim(t) for t in split_group]
+        print(group)
+
+
+clean_groups("groups_only.txt")
