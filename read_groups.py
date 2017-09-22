8c9ab87e32bc9d555a27b5c53230c5bc7bb42e84
Adam Freymiller <afreymiller10@gmail.com>
2017-09-22 01:18:04 -0500

    cleaned groups txt

--- read_groups.py
+++ read_groups.py
@@ -2,7 +2,4 @@
 file = open("groups_only.txt", "r")
 
 for line in file:
-    print(line)
-    l_strip = line.strip("]")
-    r_strip = l_strip.strip("]")
-    print(r_strip)
+    print(line.split(","))
