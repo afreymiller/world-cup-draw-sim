88b1b56836e7e9b216f893614bba6b550ca26b1b
Adam Freymiller <afreymiller10@gmail.com>
2017-09-23 10:34:12 -0500

    updating draws

--- read_groups.py
+++ read_groups.py
@@ -14,7 +14,7 @@ def clean_groups(file_name):
 
     return groups
 
-def get_team_counts(team, groups):
+def get_team_count(team, groups):
     count = 0
 
     for g in groups:
@@ -23,40 +23,39 @@ def get_team_counts(team, groups):
     
     return count
 
-def __main__():
-    groups = clean_groups("groups_only.txt")
-    arg = get_team_counts("ARG", groups)
-    aus = get_team_counts("AUS", groups)
-    bel = get_team_counts("BEL", groups)
-    bh  = get_team_counts("B&H", groups)
-    bra = get_team_counts("BRA", groups)
-    bur = get_team_counts("BUR", groups)
-    chi = get_team_counts("CHI", groups)
-    civ = get_team_counts("CIV", groups)
-    col = get_team_counts("COL", groups)
-    crc = get_team_counts("CRC", groups)
-    egy = get_team_counts("EGY", groups)
-    eng = get_team_counts("ENG", groups)
-    esp = get_team_counts("ESP", groups)
-    fra = get_team_counts("FRA", groups)
-    ger = get_team_counts("GER", groups)
-    hsv = get_team_counts("HSV", groups)
-    irn = get_team_counts("IRN", groups)
-    ita = get_team_counts("ITA", groups)
-    jpn = get_team_counts("JPN", groups)
-    kor = get_team_counts("KOR", groups)
-    ksa = get_team_counts("KSA", groups)
-    mex = get_team_counts("MEX", groups)
-    nga = get_team_counts("NGA", groups)
-    pol = get_team_counts("POL", groups)
-    por = get_team_counts("POR", groups)
-    rus = get_team_counts("RUS", groups)
-    ser = get_team_counts("SER", groups)
-    sui = get_team_counts("SUI", groups)
-    tun = get_team_counts("TUN", groups)
-    uru = get_team_counts("URU", groups)
-    usa = get_team_counts("USA", groups)
-    wal = get_team_counts("WAL", groups)
+def get_all_team_counts(groups):
+    arg = get_team_count("ARG", groups)
+    aus = get_team_count("AUS", groups)
+    bel = get_team_count("BEL", groups)
+    bh  = get_team_count("B&H", groups)
+    bra = get_team_count("BRA", groups)
+    bur = get_team_count("BUR", groups)
+    chi = get_team_count("CHI", groups)
+    civ = get_team_count("CIV", groups)
+    col = get_team_count("COL", groups)
+    crc = get_team_count("CRC", groups)
+    egy = get_team_count("EGY", groups)
+    eng = get_team_count("ENG", groups)
+    esp = get_team_count("ESP", groups)
+    fra = get_team_count("FRA", groups)
+    ger = get_team_count("GER", groups)
+    hsv = get_team_count("HSV", groups)
+    irn = get_team_count("IRN", groups)
+    ita = get_team_count("ITA", groups)
+    jpn = get_team_count("JPN", groups)
+    kor = get_team_count("KOR", groups)
+    ksa = get_team_count("KSA", groups)
+    mex = get_team_count("MEX", groups)
+    nga = get_team_count("NGA", groups)
+    pol = get_team_count("POL", groups)
+    por = get_team_count("POR", groups)
+    rus = get_team_count("RUS", groups)
+    ser = get_team_count("SER", groups)
+    sui = get_team_count("SUI", groups)
+    tun = get_team_count("TUN", groups)
+    uru = get_team_count("URU", groups)
+    usa = get_team_count("USA", groups)
+    wal = get_team_count("WAL", groups)
     
     print("ARG: " + str(arg))
     print("AUS: " + str(aus))
@@ -90,3 +89,32 @@ def __main__():
     print("URU: " + str(uru))
     print("USA: " + str(usa))
     print("WAL: " + str(wal)) 
+
+def aggregate_groups(groups):
+    group_counts = {}
+
+    for g in groups:
+        g_as_str = ''.join(g)
+        v = group_counts.get(g_as_str, 0)
+        group_counts[g_as_str] = v + 1
+
+    return group_counts
+
+def get_opponents_dict(team, groups):
+    counts = {}
+
+    for g in groups:
+        if team in g:
+            for opp in g:
+                v = counts.get(opp, 0)
+                counts[opp] = v+1
+
+    counts.pop(team, None)
+
+    return counts
+
+def __main__():
+    groups = clean_groups("groups_only.txt")
+    
+    opponents = get_opponents_dict("USA", groups)
+    print(opponents)
