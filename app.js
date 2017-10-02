2b918ae1f52369ced23643c5f2edb53731efb2e2
Adam Freymiller <afreymiller10@gmail.com>
2017-10-01 23:16:00 -0500

    figure out filtering

--- app.js
+++ app.js
@@ -12,30 +12,41 @@ new Vue({
             {'name': "G", 'One': "G1", 'Two': "G2", 'Three': "G3", 'Four': "G4"},
             {'name': "H", 'One': "H1", 'Two': "H2", 'Three': "H3", 'Four': "H4"},
         ],
-        'pots': [ { "teams": [{'name': "GER", 'path': null}, {'name': "ARG", 'path': null}, {'name': "BRA", 'path': null}, {'name': "POR", 'path': null}, 
-                             {'name': "SUI", 'path': null}, {'name': "POL", 'path': null}, {'name': "CHI", 'path': null}, {'name': "RUS", 'path': null}]}, 
-                  { "teams": [{'name': "DRC", 'path': null}, {'name': "NGA", 'path': null}, {'name': "CIV", 'path': null}, {'name': "BUR", 'path': null}, 
-                             {'name': "EGY", 'path': null}, {'name': "URU", 'path': null}, {'name': "COL", 'path': null}, {'name': "IRE", 'path': null}]},
-                  { "teams": [{'name': "MEX", 'path': null}, {'name': "CRC", 'path': null}, {'name': "USA", 'path': null}, {'name': "AUS", 'path': null}, 
-                             {'name': "IRN", 'path': null}, {'name': "KOR", 'path': null}, {'name': "JPN", 'path': null}, {'name': "KSA", 'path': null}]},
-                  { "teams": [{'name': "SWE", 'path': null}, {'name': "SER", 'path': null}, {'name': "ENG", 'path': null}, {'name': "FRA", 'path': null}, 
-                             {'name': "ESP", 'path': null}, {'name': "BEL", 'path': null}, {'name': "CRO", 'path': null}, {'name': "GRE", 'path': null}]}   
+        'pots': [ {"teams": [{'name': "GER", 'path': null, 'chosen': false}, {'name': "ARG", 'path': null, 'chosen': false}, 
+                             {'name': "BRA", 'path': null, 'chosen': false}, {'name': "POR", 'path': null, 'chosen': false}, 
+                             {'name': "SUI", 'path': null, 'chosen': false}, {'name': "POL", 'path': null, 'chosen': false}, 
+                             {'name': "CHI", 'path': null, 'chosen': false}, {'name': "RUS", 'path': null, 'chosen': false}]}, 
+                  {"teams": [{'name': "DRC", 'path': null, 'chosen': false}, {'name': "NGA", 'path': null, 'chosen': false}, 
+                             {'name': "CIV", 'path': null, 'chosen': false}, {'name': "BUR", 'path': null, 'chosen': false}, 
+                             {'name': "EGY", 'path': null, 'chosen': false}, {'name': "URU", 'path': null, 'chosen': false}, 
+                             {'name': "COL", 'path': null, 'chosen': false}, {'name': "IRE", 'path': null, 'chosen': false}]},
+                  {"teams": [{'name': "MEX", 'path': null, 'chosen': false}, {'name': "CRC", 'path': null, 'chosen': false}, 
+                             {'name': "USA", 'path': null, 'chosen': false}, {'name': "AUS", 'path': null, 'chosen': false}, 
+                             {'name': "IRN", 'path': null, 'chosen': false}, {'name': "KOR", 'path': null, 'chosen': false}, 
+                             {'name': "JPN", 'path': null, 'chosen': false}, {'name': "KSA", 'path': null, 'chosen': false}]},
+                  {"teams": [{'name': "SWE", 'path': null, 'chosen': false}, {'name': "SER", 'path': null, 'chosen': false}, 
+                             {'name': "ENG", 'path': null, 'chosen': false}, {'name': "FRA", 'path': null, 'chosen': false}, 
+                             {'name': "ESP", 'path': null, 'chosen': false}, {'name': "BEL", 'path': null, 'chosen': false}, 
+                             {'name': "CRO", 'path': null, 'chosen': false}, {'name': "GRE", 'path': null, 'chosen': false}]}   
         ]
     },
     'methods': {
         'emptyPot': function(pot) {
-            setTimeout(() => this.groups[0][pot] = this.popTeam(pot)[0], 200);
-            setTimeout(() => this.groups[1][pot] = this.popTeam(pot)[0], 400);
-            setTimeout(() => this.groups[2][pot] = this.popTeam(pot)[0], 600);
-            setTimeout(() => this.groups[3][pot] = this.popTeam(pot)[0], 800);
-            setTimeout(() => this.groups[4][pot] = this.popTeam(pot)[0], 1000);
-            setTimeout(() => this.groups[5][pot] = this.popTeam(pot)[0], 1200);
-            setTimeout(() => this.groups[6][pot] = this.popTeam(pot)[0], 1400);
-            setTimeout(() => this.groups[7][pot] = this.popTeam(pot)[0], 1600);
+            setTimeout(() => this.groups[0][pot] = this.selectTeam(pot), 200);
+            setTimeout(() => this.groups[1][pot] = this.selectTeam(pot), 400);
+            setTimeout(() => this.groups[2][pot] = this.selectTeam(pot), 600);
+            setTimeout(() => this.groups[3][pot] = this.selectTeam(pot), 800);
+            setTimeout(() => this.groups[4][pot] = this.selectTeam(pot), 1000);
+            setTimeout(() => this.groups[5][pot] = this.selectTeam(pot), 1200);
+            setTimeout(() => this.groups[6][pot] = this.selectTeam(pot), 1400);
+            setTimeout(() => this.groups[7][pot] = this.selectTeam(pot), 1600);
         },
-        'popTeam': function(pot) {
+        'selectTeam': function(pot) {
             switch (pot) {
                 case "One":
+                    let allPotTeams = this.pots[0]['teams'];
+                    let eligibleTeams = allPotTeams.filter(e => e.chosen == false);
+                    console.log(eligibleTeams);
                     return this.Pot1.splice(Math.floor(Math.random()*this.Pot1.length), 1);
                 case "Two":
                     return this.Pot2.splice(Math.floor(Math.random()*this.Pot2.length), 1);
@@ -48,7 +59,6 @@ new Vue({
             }
         },
         'teamIsChosen': function(team, index) {
-            console.log(team);
             return true;
         },
         'buttonClick': function() {
