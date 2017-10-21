de39432ca599034fc2730710ddd19b069ef332a8
Adam Freymiller <afreymiller10@gmail.com>
2017-10-21 10:25:07 -0500

    adding necessary fields

--- app.js
+++ app.js
@@ -3,39 +3,39 @@ new Vue({
     'data': {
 
         'groups': [
-            {'name': "A", 'teams': [{'name': "A1", 'url': ''}, {'name': "A2", 'url': ''},
-                                    {'name': "A3", 'url': ''}, {'name': "A4", 'url': ''}]},
-            {'name': "B", 'teams': [{'name': "B1", 'url': ''}, {'name': "B2", 'url': ''},
-                                    {'name': "B3", 'url': ''}, {'name': "B4", 'url': ''}]},
-            {'name': "C", 'teams': [{'name': "C1", 'url': ''}, {'name': "C2", 'url': ''},
-                                    {'name': "C3", 'url': ''}, {'name': "C4", 'url': ''}]},
-            {'name': "D", 'teams': [{'name': "D1", 'url': ''}, {'name': "D2", 'url': ''},
-                                    {'name': "D3", 'url': ''}, {'name': "D4", 'url': ''}]},
-            {'name': "E", 'teams': [{'name': "E1", 'url': ''}, {'name': "E2", 'url': ''},
-                                    {'name': "E3", 'url': ''}, {'name': "E4", 'url': ''}]},
-            {'name': "F", 'teams': [{'name': "F1", 'url': ''}, {'name': "F2", 'url': ''},
-                                    {'name': "F3", 'url': ''}, {'name': "F4", 'url': ''}]},
-            {'name': "G", 'teams': [{'name': "G1", 'url': ''}, {'name': "G2", 'url': ''},
-                                    {'name': "G3", 'url': ''}, {'name': "G4", 'url': ''}]},
-            {'name': "H", 'teams': [{'name': "H1", 'url': ''}, {'name': "H2", 'url': ''},
-                                    {'name': "H3", 'url': ''}, {'name': "H4", 'url': ''}]},
+            {'name': "A", 'teams': [{'name': "A1", 'final': '', 'url': ''}, {'name': "A2", 'final': '', 'url': ''},
+                                    {'name': "A3", 'final': '', 'url': ''}, {'name': "A4", 'final': '', 'url': ''}]},
+            {'name': "B", 'teams': [{'name': "B1", 'final': '', 'url': ''}, {'name': "B2", 'final': '', 'url': ''},
+                                    {'name': "B3", 'final': '', 'url': ''}, {'name': "B4", 'final': '', 'url': ''}]},
+            {'name': "C", 'teams': [{'name': "C1", 'final': '', 'url': ''}, {'name': "C2", 'final': '', 'url': ''},
+                                    {'name': "C3", 'final': '', 'url': ''}, {'name': "C4", 'final': '', 'url': ''}]},
+            {'name': "D", 'teams': [{'name': "D1", 'final': '', 'url': ''}, {'name': "D2", 'final': '', 'url': ''},
+                                    {'name': "D3", 'final': '', 'url': ''}, {'name': "D4", 'final': '', 'url': ''}]},
+            {'name': "E", 'teams': [{'name': "E1", 'final': '', 'url': ''}, {'name': "E2", 'final': '', 'url': ''},
+                                    {'name': "E3", 'final': '', 'url': ''}, {'name': "E4", 'final': '', 'url': ''}]},
+            {'name': "F", 'teams': [{'name': "F1", 'final': '', 'url': ''}, {'name': "F2", 'final': '', 'url': ''},
+                                    {'name': "F3", 'final': '', 'url': ''}, {'name': "F4", 'final': '', 'url': ''}]},
+            {'name': "G", 'teams': [{'name': "G1", 'final': '', 'url': ''}, {'name': "G2", 'final': '', 'url': ''},
+                                    {'name': "G3", 'final': '', 'url': ''}, {'name': "G4", 'final': '', 'url': ''}]},
+            {'name': "H", 'teams': [{'name': "H1", 'final': '', 'url': ''}, {'name': "H2", 'final': '', 'url': ''},
+                                    {'name': "H3", 'final': '', 'url': ''}, {'name': "H4", 'final': '', 'url': ''}]},
         ],
-        'pots': [ {"teams": [{'name': "RUS", 'path': null, 'chosen': false}, {'name': "BRA", 'path': null, 'chosen': false}, 
-                             {'name': "BEL", 'path': null, 'chosen': false}, {'name': "GER", 'path': null, 'chosen': false}, 
-                             {'name': "POL", 'path': null, 'chosen': false}, {'name': "POR", 'path': null, 'chosen': false}, 
-                             {'name': "FRA", 'path': null, 'chosen': false}, {'name': "ARG", 'path': null, 'chosen': false}]}, 
-                  {"teams": [{'name': "MEX", 'path': null, 'chosen': false}, {'name': "ENG", 'path': null, 'chosen': false}, 
-                             {'name': "ESP", 'path': null, 'chosen': false}, {'name': "URU", 'path': null, 'chosen': false}, 
-                             {'name': "COL", 'path': null, 'chosen': false}, {'name': "PER", 'path': null, 'chosen': false}, 
-                             {'name': "SUI", 'path': null, 'chosen': false}, {'name': "ITA", 'path': null, 'chosen': false}]},
-                  {"teams": [{'name': "CRC", 'path': null, 'chosen': false}, {'name': "ISL", 'path': null, 'chosen': false}, 
-                             {'name': "IRN", 'path': null, 'chosen': false}, {'name': "EGY", 'path': null, 'chosen': false}, 
-                             {'name': "CRO", 'path': null, 'chosen': false}, {'name': "DEN", 'path': null, 'chosen': false}, 
-                             {'name': "NGA", 'path': null, 'chosen': false}, {'name': "SEN", 'path': null, 'chosen': false}]},
-                  {"teams": [{'name': "KOR", 'path': null, 'chosen': false}, {'name': "KSA", 'path': null, 'chosen': false}, 
-                             {'name': "PAN", 'path': null, 'chosen': false}, {'name': "JPN", 'path': null, 'chosen': false}, 
-                             {'name': "AUS", 'path': null, 'chosen': false}, {'name': "CIV", 'path': null, 'chosen': false}, 
-                             {'name': "SER", 'path': null, 'chosen': false}, {'name': "TUN", 'path': null, 'chosen': false}]}   
+        'pots': [ {"teams": [{'name': "RUS", 'path': null, 'chosen': false, 'final': false}, {'name': "BRA", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "BEL", 'path': null, 'chosen': false, 'final': false}, {'name': "GER", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "POL", 'path': null, 'chosen': false, 'final': false}, {'name': "POR", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "FRA", 'path': null, 'chosen': false, 'final': false}, {'name': "ARG", 'path': null, 'chosen': false, 'final': false}]}, 
+                  {"teams": [{'name': "MEX", 'path': null, 'chosen': false, 'final': false}, {'name': "ENG", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "ESP", 'path': null, 'chosen': false, 'final': false}, {'name': "URU", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "COL", 'path': null, 'chosen': false, 'final': false}, {'name': "PER", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "SUI", 'path': null, 'chosen': false, 'final': false}, {'name': "ITA", 'path': null, 'chosen': false, 'final': false}]},
+                  {"teams": [{'name': "CRC", 'path': null, 'chosen': false, 'final': false}, {'name': "ISL", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "IRN", 'path': null, 'chosen': false, 'final': false}, {'name': "EGY", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "CRO", 'path': null, 'chosen': false, 'final': false}, {'name': "DEN", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "NGA", 'path': null, 'chosen': false, 'final': false}, {'name': "SEN", 'path': null, 'chosen': false, 'final': false}]},
+                  {"teams": [{'name': "KOR", 'path': null, 'chosen': false, 'final': false}, {'name': "KSA", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "PAN", 'path': null, 'chosen': false, 'final': false}, {'name': "JPN", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "AUS", 'path': null, 'chosen': false, 'final': false}, {'name': "CIV", 'path': null, 'chosen': false, 'final': false}, 
+                             {'name': "SER", 'path': null, 'chosen': false, 'final': false}, {'name': "TUN", 'path': null, 'chosen': false, 'final': false}]}   
         ]
     },
     'methods': {
@@ -56,46 +56,15 @@ new Vue({
                 index = 3;
             }
 
-            setTimeout(() => {
-                let teamChosen = this.selectTeam(pot);
-                this.groups[0].teams[index][pot] = teamChosen;
-                this.groups[0].teams[index]["url"] = teamChosen;
-            }, 200);
-            setTimeout(() => {
-                let teamChosen = this.selectTeam(pot);
-                this.groups[1].teams[index][pot] = teamChosen;
-                this.groups[1].teams[index]["url"] = teamChosen;
-            }, 400);
-            setTimeout(() => {
-                let teamChosen = this.selectTeam(pot);
-                this.groups[2].teams[index][pot] = teamChosen;
-                this.groups[2].teams[index]["url"] = teamChosen;
-            }, 600);
-            setTimeout(() => {
-                let teamChosen = this.selectTeam(pot);
-                this.groups[3].teams[index][pot] = teamChosen;
-                this.groups[3].teams[index]["url"] = teamChosen;
-            }, 800);
-            setTimeout(() => {
-                let teamChosen = this.selectTeam(pot);
-                this.groups[4].teams[index][pot] = teamChosen;
-                this.groups[4].teams[index]["url"] = teamChosen;
-            }, 1000);
-            setTimeout(() => {
-                let teamChosen = this.selectTeam(pot);
-                this.groups[5].teams[index][pot] = teamChosen;
-                this.groups[5].teams[index]["url"] = teamChosen;
-            }, 1200);
-            setTimeout(() => {
-                let teamChosen = this.selectTeam(pot);
-                this.groups[6].teams[index][pot] = teamChosen;
-                this.groups[6].teams[index]["url"] = teamChosen;
-            }, 1400);
-            setTimeout(() => {
-                let teamChosen = this.selectTeam(pot);
-                this.groups[7].teams[index][pot] = teamChosen;
-                this.groups[7].teams[index]["url"] = teamChosen;
-            }, 1600);
+            for (let i = 0; i < 8; i++) {
+                setTimeout(() => {
+                    let teamChosen = this.selectTeam(pot);
+                    
+                    this.groups[i].teams[index][pot] = teamChosen;
+                    this.groups[i].teams[index]["url"] = teamChosen;
+                    
+                }, 200*i + 200);
+            }
         },
         'selectTeam': function(pot) {
 
