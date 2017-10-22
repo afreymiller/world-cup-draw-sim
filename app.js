f622f2d89b89ea9dc1496160eacfc7f0a0a7b2cf
Adam Freymiller <afreymiller10@gmail.com>
2017-10-22 12:00:22 -0500

    it kinda works, now need to put Russia first and memoize

--- app.js
+++ app.js
@@ -3,39 +3,71 @@ new Vue({
     'data': {
 
         'groups': [
-            {'name': "A", 'teams': [{'name': "A1", 'final': '', 'url': ''}, {'name': "A2", 'final': '', 'url': ''},
-                                    {'name': "A3", 'final': '', 'url': ''}, {'name': "A4", 'final': '', 'url': ''}]},
-            {'name': "B", 'teams': [{'name': "B1", 'final': '', 'url': ''}, {'name': "B2", 'final': '', 'url': ''},
-                                    {'name': "B3", 'final': '', 'url': ''}, {'name': "B4", 'final': '', 'url': ''}]},
-            {'name': "C", 'teams': [{'name': "C1", 'final': '', 'url': ''}, {'name': "C2", 'final': '', 'url': ''},
-                                    {'name': "C3", 'final': '', 'url': ''}, {'name': "C4", 'final': '', 'url': ''}]},
-            {'name': "D", 'teams': [{'name': "D1", 'final': '', 'url': ''}, {'name': "D2", 'final': '', 'url': ''},
-                                    {'name': "D3", 'final': '', 'url': ''}, {'name': "D4", 'final': '', 'url': ''}]},
-            {'name': "E", 'teams': [{'name': "E1", 'final': '', 'url': ''}, {'name': "E2", 'final': '', 'url': ''},
-                                    {'name': "E3", 'final': '', 'url': ''}, {'name': "E4", 'final': '', 'url': ''}]},
-            {'name': "F", 'teams': [{'name': "F1", 'final': '', 'url': ''}, {'name': "F2", 'final': '', 'url': ''},
-                                    {'name': "F3", 'final': '', 'url': ''}, {'name': "F4", 'final': '', 'url': ''}]},
-            {'name': "G", 'teams': [{'name': "G1", 'final': '', 'url': ''}, {'name': "G2", 'final': '', 'url': ''},
-                                    {'name': "G3", 'final': '', 'url': ''}, {'name': "G4", 'final': '', 'url': ''}]},
-            {'name': "H", 'teams': [{'name': "H1", 'final': '', 'url': ''}, {'name': "H2", 'final': '', 'url': ''},
-                                    {'name': "H3", 'final': '', 'url': ''}, {'name': "H4", 'final': '', 'url': ''}]},
+            {'teams': [{'name': "A1", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "A2", 'fed': null, 'final': '', 'url': ''},
+                       {'name': "A3", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "A4", 'fed': null, 'final': '', 'url': ''}]},
+            {'teams': [{'name': "B1", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "B2", 'fed': null, 'final': '', 'url': ''},
+                       {'name': "B3", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "B4", 'fed': null, 'final': '', 'url': ''}]},
+            {'teams': [{'name': "C1", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "C2", 'fed': null, 'final': '', 'url': ''},
+                       {'name': "C3", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "C4", 'fed': null, 'final': '', 'url': ''}]},
+            {'teams': [{'name': "D1", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "D2", 'fed': null, 'final': '', 'url': ''},
+                       {'name': "D3", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "D4", 'fed': null, 'final': '', 'url': ''}]},
+            {'teams': [{'name': "E1", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "E2", 'fed': null, 'final': '', 'url': ''},
+                       {'name': "E3", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "E4", 'fed': null, 'final': '', 'url': ''}]},
+            {'teams': [{'name': "F1", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "F2", 'fed': null, 'final': '', 'url': ''},
+                       {'name': "F3", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "F4", 'fed': null, 'final': '', 'url': ''}]},
+            {'teams': [{'name': "G1", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "G2", 'fed': null, 'final': '', 'url': ''},
+                       {'name': "G3", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "G4", 'fed': null, 'final': '', 'url': ''}]},
+            {'teams': [{'name': "H1", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "H2", 'fed': null, 'final': '', 'url': ''},
+                       {'name': "H3", 'fed': null, 'final': '', 'url': ''}, 
+                       {'name': "H4", 'fed': null, 'final': '', 'url': ''}]},
         ],
-        'pots': [ {"teams": [{'name': "RUS", 'path': null, 'chosen': false, 'final': false}, {'name': "BRA", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "BEL", 'path': null, 'chosen': false, 'final': false}, {'name': "GER", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "POL", 'path': null, 'chosen': false, 'final': false}, {'name': "POR", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "FRA", 'path': null, 'chosen': false, 'final': false}, {'name': "ARG", 'path': null, 'chosen': false, 'final': false}]}, 
-                  {"teams": [{'name': "MEX", 'path': null, 'chosen': false, 'final': false}, {'name': "ENG", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "ESP", 'path': null, 'chosen': false, 'final': false}, {'name': "URU", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "COL", 'path': null, 'chosen': false, 'final': false}, {'name': "PER", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "SUI", 'path': null, 'chosen': false, 'final': false}, {'name': "ITA", 'path': null, 'chosen': false, 'final': false}]},
-                  {"teams": [{'name': "CRC", 'path': null, 'chosen': false, 'final': false}, {'name': "ISL", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "IRN", 'path': null, 'chosen': false, 'final': false}, {'name': "EGY", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "CRO", 'path': null, 'chosen': false, 'final': false}, {'name': "DEN", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "NGA", 'path': null, 'chosen': false, 'final': false}, {'name': "SEN", 'path': null, 'chosen': false, 'final': false}]},
-                  {"teams": [{'name': "KOR", 'path': null, 'chosen': false, 'final': false}, {'name': "KSA", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "PAN", 'path': null, 'chosen': false, 'final': false}, {'name': "JPN", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "AUS", 'path': null, 'chosen': false, 'final': false}, {'name': "CIV", 'path': null, 'chosen': false, 'final': false}, 
-                             {'name': "SER", 'path': null, 'chosen': false, 'final': false}, {'name': "TUN", 'path': null, 'chosen': false, 'final': false}]}   
+        'pots': [ {"teams": [{'name': "RUS", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "BRA", 'fed': 'CONMEBOL', 'chosen': false, 'final': false}, 
+                             {'name': "BEL", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "GER", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "POL", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "POR", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "FRA", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "ARG", 'fed': 'UEFA', 'chosen': false, 'final': false}]}, 
+                  {"teams": [{'name': "MEX", 'fed': 'CONCACAF', 'chosen': false, 'final': false}, 
+                             {'name': "ENG", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "ESP", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "URU", 'fed': 'CONMEBOL', 'chosen': false, 'final': false}, 
+                             {'name': "COL", 'fed': 'CONMEBOL', 'chosen': false, 'final': false}, 
+                             {'name': "PER", 'fed': 'CONMEBOL', 'chosen': false, 'final': false}, 
+                             {'name': "SUI", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "ITA", 'fed': 'UEFA', 'chosen': false, 'final': false}]},
+                  {"teams": [{'name': "CRC", 'fed': 'CONCACAF', 'chosen': false, 'final': false}, 
+                             {'name': "ISL", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "IRN", 'fed': 'AFC', 'chosen': false, 'final': false}, 
+                             {'name': "EGY", 'fed': 'CAF', 'chosen': false, 'final': false}, 
+                             {'name': "CRO", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "DEN", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "NGA", 'fed': 'CAF', 'chosen': false, 'final': false}, 
+                             {'name': "SEN", 'fed': 'CAF', 'chosen': false, 'final': false}]},
+                  {"teams": [{'name': "KOR", 'fed': 'AFC', 'chosen': false, 'final': false}, 
+                             {'name': "KSA", 'fed': 'AFC', 'chosen': false, 'final': false}, 
+                             {'name': "PAN", 'fed': 'CONCACAF', 'chosen': false, 'final': false}, 
+                             {'name': "JPN", 'fed': 'AFC', 'chosen': false, 'final': false}, 
+                             {'name': "AUS", 'fed': 'AFC', 'chosen': false, 'final': false}, 
+                             {'name': "CIV", 'fed': 'CAF', 'chosen': false, 'final': false}, 
+                             {'name': "SER", 'fed': 'UEFA', 'chosen': false, 'final': false}, 
+                             {'name': "TUN", 'fed': 'CAF', 'chosen': false, 'final': false}]}   
         ]
     },
     'methods': {
@@ -58,14 +90,11 @@ new Vue({
 
             for (let i = 0; i < 8; i++) {
                 setTimeout(() => {
-                    let teamChosen = this.selectTeam(pot);
-                    
-                    this.groups[i].teams[index]["url"] = teamChosen;
-                    
-                }, 200*i + 200);
+                    this.selectTeam(pot, i);
+                }, 1000*i + 200);
             }
         },
-        'selectTeam': function(pot) {
+        'selectTeam': function(pot, groupIndex) {
 
             /* There should be a more ES6 friendly way of doing the pot check */
 
@@ -87,10 +116,24 @@ new Vue({
                 index = 3;
             }
 
+            let canAddTeam = false;
+            let drawnTeam = null;
             let allPotTeams = this.pots[index]['teams'];
             let eligibleTeams = allPotTeams.filter(e => e.chosen == false);
-            let drawnIndex = Math.floor(Math.random()*eligibleTeams.length);
-            let drawnTeam = eligibleTeams[drawnIndex];
+
+            while (!canAddTeam) {
+                
+                let drawnIndex = Math.floor(Math.random()*eligibleTeams.length);
+                drawnTeam = eligibleTeams[drawnIndex];
+
+                this.groups[groupIndex].teams[index].fed = drawnTeam.fed;
+
+                canAddTeam = this.groupIsValid(this.groups[groupIndex]);
+            }
+
+            this.groups[groupIndex].teams[index]["url"] = drawnTeam.name;
+            
+            console.log(drawnTeam.name + " added to Group " + groupIndex + " spot " + index);
 
             for (let team of allPotTeams) {
                 if (team.name === drawnTeam.name) {
@@ -98,17 +141,36 @@ new Vue({
                 }
             }
 
-            return drawnTeam.name;
+        },
+        'groupIsValid': function(group) {
+            const DIST = {"UEFA": 0, "CAF": 0, "AFC": 0, "CONMEBOL": 0, "CONCACAF": 0};
+         
+            for (let team of group.teams) {
+                const federation = team.fed; 
+                DIST[federation] += 1;
+            };
 
+            return (DIST.UEFA <= 2 && DIST.CAF <= 1 && DIST.AFC <= 1 && DIST.CONMEBOL <= 1 && DIST.CONCACAF <= 1);
+        },
+
+        'distributionIsValid': function() {
+            let isValid = true;
+
+            for (let group of this.groups) {
+                isValid = isValid && this.groupIsValid(group);
+            }
+
+            alert(isValid);
+            return isValid;
         },
         'teamIsChosen': function(team, potIndex, teamIndex) {
             return this.pots[potIndex]["teams"][teamIndex]["chosen"];
         },
         'buttonClick': function() {
             setTimeout(() => this.emptyPot("One"), 0);
-            setTimeout(() => this.emptyPot("Two"), 1600);
-            setTimeout(() => this.emptyPot("Three"), 3200);
-            setTimeout(() => this.emptyPot("Four"), 4800);
+            setTimeout(() => this.emptyPot("Two"), 10000);
+            setTimeout(() => this.emptyPot("Three"), 20000);
+            setTimeout(() => this.emptyPot("Four"), 30000);
 
         },
         'getImgPath': function(team) {
