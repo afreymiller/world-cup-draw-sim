e6f78bd7711a5d4e3995d14dd89869b94ddde6fb
Adam Freymiller <afreymiller10@gmail.com>
2017-11-04 22:23:51 -0500

    up and running

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'pug')
+app.set('views', './client/views')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.render('index'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
