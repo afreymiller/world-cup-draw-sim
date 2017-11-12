d78db30fa040f1183cde5fb204715dab86069963
Adam Freymiller <afreymiller10@gmail.com>
2017-11-11 21:56:24 -0600

    updating ports

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 8000
 
 app.set('view engine', 'pug')
 app.set('views', './client/views')
@@ -9,4 +9,4 @@ app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('index'))
 
-app.listen(port, () => console.log('App listening on port 3000'))
+app.listen(port, () => console.log('App listening on port 8000'))
